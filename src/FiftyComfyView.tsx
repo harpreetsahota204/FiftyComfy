6b08f01ab00d4d9842452408ce40ba1d65a1544e
harpreetsahota204 <harpreetsahota204@users.noreply.github.com>
2026-02-13 12:34:38 -0600

    set canvas CSS to 100% width/height so it fills panel before doResize syncs pixel buffer

--- src/FiftyComfyView.tsx
+++ src/FiftyComfyView.tsx
@@ -326,7 +326,7 @@ export default function FiftyComfyView() {
   },
     React.createElement("canvas", {
       ref: canvasRef,
-      style: { position: "absolute", top: 0, left: 0 },
+      style: { position: "absolute", top: 0, left: 0, width: "100%", height: "100%" },
     }),
     React.createElement("div", { style: toolbarCss },
       React.createElement("button", {
